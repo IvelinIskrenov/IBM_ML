a6443ca4cc9464cdd0109cb777d5f5020774d5d1
IvelinIskrenov <ivelin_iskrenov@abv.bg>
2025-09-20 00:01:46 +0300

    LinearRegression testing

--- ML_LinearRegression.py
+++ ML_LinearRegression.py
@@ -12,6 +12,8 @@ def main():
     cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
     cdf.sample(9)
     
+    #learn example (testing)
+    """
     viz = cdf[['CYLINDERS','ENGINESIZE','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
     viz.hist() #generating histogram for each col
     plt.show()
@@ -26,10 +28,40 @@ def main():
     plt.ylabel("Emission")
     plt.xlim(0,27) #zoom
     plt.show()
-
+    
     plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS,  color='blue')
     plt.xlabel("CYLINDERS")
     plt.ylabel("CO2 Emission")
     plt.show()
+    """
+    #extracting the input feature and target output variables - X and y from the dataset.
+    X = cdf.ENGINESIZE.to_numpy()
+    y = cdf.CO2EMISSIONS.to_numpy()
+    
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+    
+    type(X_train), np.shape(X_train), np.shape(X_train)
+    
+    from sklearn import linear_model
+    #create a model object
+    regressor = linear_model.LinearRegression()
+    
+    #shape (n_observations, n_features).
+    #need to reshape it - We can let it infer the number of observations using '-1'.
+    regressor.fit(X_train.reshape(-1, 1), y_train)
+    
+    # Print the coefficients
+    print ('Coefficients: ', regressor.coef_[0]) # with simple linear regression there is only one coefficient, here we extract it from the 1 by 1 array.
+    print ('Intercept: ',regressor.intercept_)
+    
+    plt.scatter(X_train, y_train,  color='blue')
+    plt.plot(X_train, regressor.coef_ * X_train + regressor.intercept_, '-r')
+    plt.xlabel("Engine size")
+    plt.ylabel("Emission")
+    plt.show()
+    
+    
+    
 if __name__ == "__main__":
     main()
