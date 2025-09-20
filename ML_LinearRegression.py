8e9c5571ad029e90a9e3fce8b41e1a3223a13a39
IvelinIskrenov <ivelin_iskrenov@abv.bg>
2025-09-20 09:21:17 +0300

    Finished and add 1 more Linear regression for compare

--- ML_LinearRegression.py
+++ ML_LinearRegression.py
@@ -48,11 +48,11 @@ def main():
     regressor = linear_model.LinearRegression()
     
     #shape (n_observations, n_features).
-    #need to reshape it - We can let it infer the number of observations using '-1'.
-    regressor.fit(X_train.reshape(-1, 1), y_train)
+    #need to reshape it - We can let it infer the number of observations using '-1'. (From 1D to 2D)
+    regressor.fit(X_train.reshape(-1, 1), y_train) # fit -> calculate theta (minimizing MSE)
     
-    # Print the coefficients
-    print ('Coefficients: ', regressor.coef_[0]) # with simple linear regression there is only one coefficient, here we extract it from the 1 by 1 array.
+    #print the coefficients
+    print ('Coefficients: ', regressor.coef_[0]) #with simple linear regression there is only one coefficient, here we extract it from the 1 by 1 array.
     print ('Intercept: ',regressor.intercept_)
     
     plt.scatter(X_train, y_train,  color='blue')
@@ -61,7 +61,34 @@ def main():
     plt.ylabel("Emission")
     plt.show()
     
+    from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+    # Use the predict method to make test predictions
+    y_test_ = regressor.predict(X_test.reshape(-1,1))
+
+    # Evaluation
+    print("Mean absolute error: %.2f" % mean_absolute_error(y_test, y_test_))
+    print("Mean squared error: %.2f" % mean_squared_error(y_test, y_test_))
+    print("Root mean squared error: %.2f" % np.sqrt(mean_squared_error(y_test, y_test_)))
+    print("R2-score: %.2f" % r2_score(y_test, y_test_))
     
+    #example 2
+    plt.scatter(X_test, y_test,  color='blue')
+    plt.plot(X_test, regressor.coef_ * X_test + regressor.intercept_, '-r')
+    plt.xlabel("Engine size")
+    plt.ylabel("Emission")
+    
+    X = cdf.FUELCONSUMPTION_COMB.to_numpy()
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+    
+    regr = linear_model.LinearRegression()
+    regr.fit(X_train.reshape(-1, 1), y_train)
+    
+    y_test_ = regr.predict(X_test.reshape(-1,1))
+    
+    print("Mean squared error: %.2f" % mean_squared_error(y_test, y_test_))
+    print("R2-score: %.2f" % r2_score(y_test, y_test_))
+    
+    #LinearRegression 2 is more efficient then 1
     
 if __name__ == "__main__":
     main()
