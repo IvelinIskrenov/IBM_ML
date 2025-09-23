56f71f7dac315b07b4eaf3fc9e8010f9dc80f9ea
IvelinIskrenov <ivelin_iskrenov@abv.bg>
2025-09-23 09:28:35 +0300

    train(), accurancy() added, modify the hyperparams of LR

--- ML_LogisticRegression.py
+++ ML_LogisticRegression.py
@@ -6,6 +6,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split #!
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
 class Logistic_model:
     def __init__(self):
@@ -20,6 +23,7 @@ class Logistic_model:
         self.y_test = None
         self.logisticRegressor = None
         self.yhat = None
+        self.coefficients = None
         
         
     def loadData(self):
@@ -31,10 +35,10 @@ class Logistic_model:
     
     def preprocessing(self):
         print(self.churn_df.head(9))
-        self.churn_df = self.churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip', 'churn']]
+        self.churn_df = self.churn_df[['tenure', 'income', 'ed', 'equip', 'churn']]#, 'equip'
         self.churn_df['churn'] = self.churn_df['churn'].astype('int') #set the predicted value as type int
         
-        self.X = np.asarray(self.churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip']])
+        self.X = np.asarray(self.churn_df[['tenure',  'income', 'ed', 'equip']]) # ,'age', 'address',||, 'employ' ,,'wireless'
         self.y = np.asarray(self.churn_df['churn'])
         
         self.X_norm = StandardScaler().fit(self.X).transform(self.X)
@@ -45,19 +49,38 @@ class Logistic_model:
             )
         
     def train(self):
-        self.logisticRegressor = LogisticRegression().fit(self.X_train, self.y_train)
+        self.logisticRegressor = LogisticRegression(solver='lbfgs', penalty='l2', C=1.0, max_iter=2).fit(self.X_train, self.y_train)
         
         self.yhat = self.logisticRegressor.predict(self.X_test)
         print(self.yhat[:10])
+        
+        yhat_prob = self.logisticRegressor.predict_proba(self.X_test) #see the probability
+        print(yhat_prob[:10])
+        
+        coefficients = pd.Series(self.logisticRegressor.coef_[0], index=self.churn_df.columns[:-1])
+        coefficients.sort_values().plot(kind='barh')
+        plt.title("Feature Coefficients in Logistic Regression Churn Model")
+        plt.xlabel("Coefficient Value")
+        plt.show() 
+        
+        print(log_loss(self.y_test, yhat_prob))
     
+    def accurancy(self):
+        y_pred = self.logisticRegressor.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        print("Accuracy: {:.2f}%".format(accuracy * 100))
+        
     def run(self):
         self.loadData()
         self.preprocessing()
         self.splitDataSet()
         self.train()
+        self.accurancy()
+        
         
 if __name__ == '__main__':
     model = Logistic_model()
     model.run()
+        
         
         
