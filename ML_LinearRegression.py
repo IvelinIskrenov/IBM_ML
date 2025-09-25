22ace83fae32e1e709822f7cdc0e4343295d2cd5
IvelinIskrenov <ivelin_iskrenov@abv.bg>
2025-09-25 12:08:27 +0300

    LinearRegression.py - Adding little more comments and summery for more easier understandable

--- ML_LinearRegression.py
+++ ML_LinearRegression.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
+#class to analyze fuel cons. data and predict CO2 emissions
 class FuelEmissions:
     def __init__(self, url=None):
         if url is None:
@@ -16,104 +17,90 @@ class FuelEmissions:
         self.regr = None
 
     def load_data(self):
+        """
+        Loads the data from a CSV file.
+        """
         url = self.url
-        df = pd.read_csv(url) #load in dataframe
+        #Read the data file into a DataFrame.
+        df = pd.read_csv(url) 
         self.df = df
 
-        df.sample(5)
         print(df.sample(5))
 
+        #Selecting only the important cols
         cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
-        cdf.sample(9)
         self.cdf = cdf
 
-    def show_examples(self):
-        #learn example (testing)
-        """
-        viz = cdf[['CYLINDERS','ENGINESIZE','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
-        viz.hist() #generating histogram for each col
-        plt.show()
-        
-        plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue') #creating dot diagram X = FC_COMB, Y = CO2E.
-        plt.xlabel("FUELCONSUMPTION_COMB")
-        plt.ylabel("Emission")
-        plt.show()
-        
-        plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
-        plt.xlabel("Engine size")
-        plt.ylabel("Emission")
-        plt.xlim(0,27) #zoom
-        plt.show()
-        
-        plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS,  color='blue')
-        plt.xlabel("CYLINDERS")
-        plt.ylabel("CO2 Emission")
-        plt.show()
-        """
-        pass
-
     def run_regression_enginesize(self):
-        #extracting the input feature and target output variables - X and y from the dataset.
+        """
+        Predicts emissions based on engine size.
+        """
         X = self.cdf.ENGINESIZE.to_numpy()
         y = self.cdf.CO2EMISSIONS.to_numpy()
         
+        #Split the data into training and testing sets.
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         
-        type(X_train), np.shape(X_train), np.shape(X_train)
-        
-        #create a model object
         regressor = linear_model.LinearRegression()
         self.regressor = regressor
         
-        #shape (n_observations, n_features).
-        #need to reshape it - We can let it infer the number of observations using '-1'. (From 1D to 2D)
-        regressor.fit(X_train.reshape(-1, 1), y_train) # fit -> calculate theta (minimizing MSE)
+        #train the model &&
+        #reshape the data to fit the model's requirements.
+        regressor.fit(X_train.reshape(-1, 1), y_train)
         
-        #print the coefficients
-        print ('Coefficients: ', regressor.coef_[0]) #with simple linear regression there is only one coefficient, here we extract it from the 1 by 1 array.
+        print ('Coefficients: ', regressor.coef_[0])
         print ('Intercept: ',regressor.intercept_)
         
-        plt.scatter(X_train, y_train,  color='blue')
+        #Plot the traing data
+        plt.scatter(X_train, y_train, color='blue')
         plt.plot(X_train, regressor.coef_ * X_train + regressor.intercept_, '-r')
         plt.xlabel("Engine size")
         plt.ylabel("Emission")
         plt.show()
         
-        # Use the predict method to make test predictions
+        #use the trained model to make predictions on the test data.
         y_test_ = regressor.predict(X_test.reshape(-1,1))
 
-        # Evaluation
+        #evaluate how well the model performed
         print("Mean absolute error: %.2f" % mean_absolute_error(y_test, y_test_))
         print("Mean squared error: %.2f" % mean_squared_error(y_test, y_test_))
         print("Root mean squared error: %.2f" % np.sqrt(mean_squared_error(y_test, y_test_)))
         print("R2-score: %.2f" % r2_score(y_test, y_test_))
         
-        #example 2
-        plt.scatter(X_test, y_test,  color='blue')
+        #Plot the test data
+        plt.scatter(X_test, y_test, color='blue')
         plt.plot(X_test, regressor.coef_ * X_test + regressor.intercept_, '-r')
         plt.xlabel("Engine size")
         plt.ylabel("Emission")
         plt.show()
 
     def run_regression_fuelconsumption(self):
+        """
+        Predicts emissions based on fuel consumption.
+        """
+        #set up the input and output data.
         X = self.cdf.FUELCONSUMPTION_COMB.to_numpy()
         y = self.cdf.CO2EMISSIONS.to_numpy()
+        #Split the data into training and testing sets.
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         
+        #Create and train the model.
         regr = linear_model.LinearRegression()
         self.regr = regr
         regr.fit(X_train.reshape(-1, 1), y_train)
         
         y_test_ = regr.predict(X_test.reshape(-1,1))
         
+        #evaluate the model.
         print("Mean squared error: %.2f" % mean_squared_error(y_test, y_test_))
         print("R2-score: %.2f" % r2_score(y_test, y_test_))
-        
-        #LinearRegression 2 is more efficient then 1
 
     def run(self):
+        """
+        Runs the entire analysis process.
+        """
         self.load_data()
-        # self.show_examples()  # kept as method to preserve the triple-quoted block; not called
+        # self.show_examples()  
         self.run_regression_enginesize()
         self.run_regression_fuelconsumption()
 
@@ -123,4 +110,5 @@ def main():
     fm.run()
 
 if __name__ == "__main__":
+    #runs if  script is executable
     main()
