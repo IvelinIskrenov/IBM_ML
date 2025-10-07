899ccc63d57d2ee5bb5dfdfa99ffcdda2f2da197
IvelinIskrenov <ivelin_iskrenov@abv.bg>
2025-10-07 11:39:52 +0300

    ML_SVM -> improve the hyperParms of the Decision tree which is compared with SVM, timer on methods added

--- Supervised Learning models/ML_SVM.py
+++ Supervised Learning models/ML_SVM.py
@@ -6,9 +6,20 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import LinearSVC
 from sklearn.utils.class_weight import compute_sample_weight
 from sklearn.metrics import roc_auc_score
+import time
 
 import warnings
 warnings.filterwarnings('ignore')
+
+#from stackoverflow
+def timed(func):
+    def wrapper(self, *args, **kwargs):
+        t0 = time.perf_counter()
+        result = func(self, *args, **kwargs)
+        t1 = time.perf_counter()
+        print(f"{func.__name__} took {t1-t0:.3f}s")
+        return result
+    return wrapper
 
 class CreditCardFraudDetection():
     '''Build Decision Tree model and SVM model, then we compare it to see which model works better'''
@@ -23,14 +34,20 @@ class CreditCardFraudDetection():
         self.model_SVM = None
         self.model_DecisionTree = None
         
+    @timed    
     def load_data(self):
         '''Load the data from the url'''
         if self.data == None:
             url= "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/creditcard.csv"
             self.data=pd.read_csv(url)
-    
+        print("Data loaded")
+        
+    @timed 
     def data_analysis(self):
-        '''finds the best feature that correlate with the target and visul. the class sample counts'''
+        '''
+        finds the best feature that correlate with the target and visul. the class sample counts
+        finds top 6 features
+        '''
         # get the set of distinct classes
         labels = self.data['Class'].unique()
 
@@ -46,28 +63,45 @@ class CreditCardFraudDetection():
         correlation_values = self.data.corr()['Class'].drop('Class')
         correlation_values.plot(kind='barh', figsize=(10, 6))
         plt.show()
-
+        
+        
+        print("Top 6 correlation features: ")
+        correlation_values = abs(self.data.corr()['Class']).drop('Class')
+        correlation_values = correlation_values.sort_values(ascending=False)[:6]
+        print(correlation_values)
+        
+    @timed 
     def preprocessing(self):
         #standardize features by removing the mean and scaling to unit variance
         self.data.iloc[:, 1:30] = StandardScaler().fit_transform(self.data.iloc[:, 1:30])
         data_matrix = self.data.values
         
-        self.X = data_matrix[:, 1:30]
+        #self.X = data_matrix[:, 1:30]
+        #set the features from the data_analysis()
+        self.X = data_matrix[:,[3,10,12,14,16,17]]
 
         self.y = data_matrix[:, 30]
 
         self.X = normalize(self.X, norm="l1")
         
+        print("Preprocess finished !!!")
+     
+    @timed    
     def split_data(self):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
-        
+        print("Split data done !!!")
+    
+    @timed     
     def buildTrainSVM(self):
+        '''Builds and trains a balanced Linear SVM model'''
         self.model_SVM = LinearSVC(class_weight='balanced', random_state=31, loss="hinge", fit_intercept=False)
         self.model_SVM.fit(self.X_train, self.y_train)
-        
+    
+    @timed     
     def buildTrainDecisionTree(self):
+        '''Builds and trains a Decision Tree (entropy, max_depth=4) with balanced sample weights'''
         w_train = compute_sample_weight('balanced', self.y_train)
-        self.model_DecisionTree = DecisionTreeClassifier(criterion='gini', max_depth=10, random_state=35)
+        self.model_DecisionTree = DecisionTreeClassifier(criterion='entropy', max_depth=4, random_state=35)
         self.model_DecisionTree.fit(self.X_train, self.y_train, sample_weight=w_train)
     
     #roc_auc_score - how good models distinguish positive from negative ones
@@ -91,8 +125,24 @@ class CreditCardFraudDetection():
         self.buildTrainDecisionTree()
         self.evaluationDecisionTree()
         self.buildTrainSVM()
-        self.evaluationSVM()
-    
+        self.evaluationSVM() 
+   
 if __name__ == '__main__':
     model = CreditCardFraudDetection()
     model.run()
+    
+    
+    #before feature selecting
+    #Decision Tree ROC-AUC score : 0.912
+    #SVM ROC-AUC score: 0.984
+    
+    #after feature selecting
+    #Decision Tree ROC-AUC score : 0.927 max depth 10 
+    #SVM ROC-AUC score: 0.929
+    
+    #testing other hyperparams
+    #Decision Tree ROC-AUC score : 0.927 max depth 10 -> 0.946 max depth 4 -> (gini->entropy) 0.952
+    
+    #With a larger set of features, SVM performed relatively better in comparison to the Decision Trees
+    #Decision Trees benefited from feature selection and performed better
+    #SVMs may require higher feature dimensionality to create an efficient decision hyperplane
