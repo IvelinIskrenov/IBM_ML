53a6cda5a454a8f2240da19c560391a31c2a5fd8
IvelinIskrenov <ivelin_iskrenov@abv.bg>
2025-10-08 11:24:35 +0300

    ML_KNN added feature selection and summeries for more readable

--- Supervised Learning models/ML_KNN.py
+++ Supervised Learning models/ML_KNN.py
@@ -29,7 +29,11 @@ class KnnModel():
             self.data = pd.read_csv(url)
 
     def data_analysis(self):
-        self.data['custcat'].value_counts() #distribution of the data set
+        '''1. print distribution of data set
+           2. print correlation matrix
+           3. correlation values
+        '''
+        print(self.data['custcat'].value_counts()) #distribution of the data set
         
         correlation_matrix = self.data.corr()
 
@@ -39,25 +43,32 @@ class KnnModel():
         
         correlation_values = abs(self.data.corr()['custcat'].drop('custcat')).sort_values(ascending=False)
         print(correlation_values) 
-        
-        self.X = self.data.drop('custcat',axis=1)
-        self.y = self.data['custcat']     
+             
     
     def preprocessing(self):
+        '''Standartising the values'''
+        #keep the features with corrValue > 0.15
+        correlation_values = abs(self.data.corr()['custcat'].drop('custcat')).sort_values(ascending=False)
+        features_to_keep = correlation_values[correlation_values > 0.15].index.tolist()
+        self.X = self.data[features_to_keep] 
+        self.y = self.data['custcat']
+        
         self.X_norm = StandardScaler().fit_transform(self.X)
     
     def split_data(self):
+        '''Splits data into train and test set'''
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X_norm, self.y, test_size=0.2, random_state=4)
         
     def build_train_KNN(self):
-        
+        '''Build and train the KNN model with k = 3 hyperparam'''
         #hyperparam k
-        k = 3
+        k = 9 # best accuracy with k = 94  is 0.445
         #Train Model and Predict  
         knn_classifier = KNeighborsClassifier(n_neighbors=k)
         self.model_KNN = knn_classifier.fit(self.X_train, self.y_train)
         
     def evaluatuion(self):
+        '''evaluation with accuracy_score'''
         yhat = self.model_KNN.predict(self.X_test)
         print("Test set Accuracy: ", accuracy_score(self.y_test, yhat))
        
@@ -68,13 +79,14 @@ class KnnModel():
         std_acc = np.zeros((Ks))
         for n in range(1,Ks+1): 
             KNN_tuning_model = KNeighborsClassifier(n_neighbors = n).fit(self.X_train, self.y_train)
-            yhat = KNN_tuning_model.predict(self.X_train) #x-set
-            acc[n-1] = accuracy_score(self.y_train, yhat) #Y-test
-            std_acc[n-1] = np.std(yhat==self.y_train)/np.sqrt(yhat.shape[0]) #y-test
+            yhat = KNN_tuning_model.predict(self.X_test) #
+            acc[n-1] = accuracy_score(self.y_test, yhat) #
+            std_acc[n-1] = np.std(yhat==self.y_test)/np.sqrt(yhat.shape[0]) #
         
         self.plot_k_tuning(Ks, acc, std_acc)
 
     def plot_k_tuning(self,Ks,acc,std_acc):
+        '''Print the calculates from k_tuning() method which gives the best k value'''
         plt.plot(range(1,Ks+1),acc,'g')
         plt.fill_between(range(1,Ks+1),acc - 1 * std_acc,acc + 1 * std_acc, alpha=0.10)
         plt.legend(('Accuracy value', 'Standard Deviation'))
