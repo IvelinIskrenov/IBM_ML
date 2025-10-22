5de81444a8030adf11f13219f8c5d25eac51c8d5
IvelinIskrenov <ivelin_iskrenov@abv.bg>
2025-10-22 23:15:42 +0300

    Update K-means, CustomerSegmentation_Kmeans vars

--- Unsupervised Learning models/CustomerSegmentation_Kmeans.py
+++ Unsupervised Learning models/CustomerSegmentation_Kmeans.py
@@ -17,57 +17,57 @@ class CustomerSegmentation():
         n_init : n times - k-means Alg. will be run with different centroid seeds
     '''
     def __init__(self):
-        self.data = None
-        self.std_data = None
-        self.n_clusters = None # this is the K in k-means
-        self.n_init = None
-        self.X = None
-        self.y = None
-        self.k_means = None
-        self.labels = None
+        self.__data = None
+        self.__std_data = None
+        self.__n_clusters = None # this is the K in k-means
+        self.__n_init = None
+        self.__X = None
+        self.__y = None
+        self.__k_means = None
+        self.__labels = None
         
         
     def load_data(self):
-        if self.data == None:
-            self.data = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%204/data/Cust_Segmentation.csv")
+        if self.__data == None:
+            self.__data = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%204/data/Cust_Segmentation.csv")
         
     def preprocessing(self):
         '''Drop the address col to remains only numerical data && Standartizing the data'''
-        self.data = self.data.drop('Address', axis=1)
-        self.data = self.data.dropna()
-        print(self.data.info())
+        self.__data = self.__data.drop('Address', axis=1)
+        self.__data = self.__data.dropna()
+        print(self.__data.info())
         
-        self.X = self.data.values[:,1:] #leaves out - Customer ID
-        self.std_data = StandardScaler().fit_transform(self.X)
+        self.__X = self.__data.values[:,1:] #leaves out - Customer ID
+        self.__std_data = StandardScaler().fit_transform(self.__X)
               
     def visualizing_data(self):
         '''Visualize the data clusters i plot'''
         np.random.seed(0)
-        plt.scatter(self.X[:, 0], self.X[:, 1], marker='.',alpha=0.3,ec='k',s=80)
+        plt.scatter(self.__X[:, 0], self.__X[:, 1], marker='.',alpha=0.3,ec='k',s=80)
         plt.show()
 
     def train_model_Kmeans(self,n__clusters = 3, n__init = 12):
         '''Train and build k-means++ with 3 clusters and 12 inits (default)'''
-        self.n_clusters = n__clusters
-        self.n_init = n__init
+        self.__n_clusters = n__clusters
+        self.__n_init = n__init
         
-        self.k_means = KMeans(init="k-means++", n_clusters = self.n_clusters, n_init = self.n_init)
-        self.k_means.fit(self.X)
-        self.labels = self.k_means.labels_
+        self.__k_means = KMeans(init="k-means++", n_clusters = self.__n_clusters, n_init = self.__n_init)
+        self.__k_means.fit(self.__X)
+        self.__labels = self.__k_means.labels_
         
     def data_exploration(self):
         '''Add new col "Clus_km" && show the clusters in 2d'''
-        self.data["Clus_km"] = self.labels
-        self.data.groupby('Clus_km').mean()
+        self.__data["Clus_km"] = self.__labels
+        self.__data.groupby('Clus_km').mean()
         
-        area = np.pi * ( self.X[:, 1])**2  
-        plt.scatter(self.X[:, 0], self.X[:, 3], s=area, c=self.labels.astype(float), cmap='tab10', ec='k',alpha=0.5)
+        area = np.pi * ( self.__X[:, 1])**2  
+        plt.scatter(self.__X[:, 0], self.__X[:, 3], s=area, c=self.__labels.astype(float), cmap='tab10', ec='k',alpha=0.5)
         plt.xlabel('Age', fontsize=18)
         plt.ylabel('Income', fontsize=16)
         plt.show()  
     
     def visual_plot_3D(self):
-        fig = px.scatter_3d(self.X, x=1, y=0, z=3, opacity=0.7, color=self.labels.astype(float))
+        fig = px.scatter_3d(self.__X, x=1, y=0, z=3, opacity=0.7, color=self.__labels.astype(float))
 
         fig.update_traces(marker=dict(size=5, line=dict(width=.25)), showlegend=False)
         fig.update_layout(coloraxis_showscale=False, width=1000, height=800, scene=dict(
@@ -80,7 +80,7 @@ class CustomerSegmentation():
     
     def create_profile_foreach_Group(self):
         '''considering the common characteristics of each cluster based on your observations'''
-        cust_df_sub = self.data[['Age', 'Edu','Income','Clus_km']].copy() 
+        cust_df_sub = self.__data[['Age', 'Edu','Income','Clus_km']].copy() 
         sns.pairplot(cust_df_sub, hue='Clus_km', palette='viridis', diag_kind='kde') 
         plt.suptitle('Pairwise Scatter Plot with K-means Clusters', y=1.02)
         plt.show()
--- Unsupervised Learning models/K-means.py
+++ Unsupervised Learning models/K-means.py
@@ -18,72 +18,75 @@ class K_means():
         The final results will be the best output of n_init consecutive runs in terms of inertia
     '''
     def __init__(self):
-        self.n_clusters = None # this is the K in k-means
-        self.n_init = None
-        self.X = None
-        self.y = None
-        self.model = None
-        self.k_means_labels = None
-        self.k_means_cluster_centers = None
+        self.__n_clusters = None # this is the K in k-means
+        self.__n_init = None
+        self.__X = None
+        self.__y = None
+        self.__model = None
+        self.__k_means_labels = None
+        self.__k_means_cluster_centers = None
         
         
     def create_syntetic_data(self):
         '''Create syntetic data with make_blobs function'''
-        self.X, self.y = make_blobs(n_samples=5000, centers=[[4,4], [-2, -1], [2, -3], [1, 1]], cluster_std=0.9)
+        self.__X, self.__y = make_blobs(n_samples=5000, centers=[[4,4], [-2, -1], [2, -3], [1, 1]], cluster_std=0.9)
     
     def visualizing_data(self):
         '''Visualize the data clusters i plot'''
         np.random.seed(0)
-        plt.scatter(self.X[:, 0], self.X[:, 1], marker='.',alpha=0.3,ec='k',s=80)
+        plt.scatter(self.__X[:, 0], self.__X[:, 1], marker='.',alpha=0.3,ec='k',s=80)
         plt.show()
 
     def train_model(self,n__clusters = 4, n__init = 12):
-        self.n_clusters = n__clusters
-        self.n_init = n__init
+        self.__n_clusters = n__clusters
+        self.__n_init = n__init
         
-        self.model = KMeans(init = "k-means++", n_clusters = self.n_clusters, n_init = self.n_init)
-        self.model.fit(self.X)
+        self.__model = KMeans(init = "k-means++", n_clusters = self.__n_clusters, n_init = self.__n_init)
+        self.__model.fit(self.__X)
         
-        self.k_means_labels = self.model.labels_
-        print(self.k_means_labels)
+        self.__k_means_labels = self.__model.labels_
+        print(self.__k_means_labels)
         
-        self.k_means_cluster_centers = self.model.cluster_centers_
-        print(self.k_means_cluster_centers)
+        self.__k_means_cluster_centers = self.__model.cluster_centers_
+        print(self.__k_means_cluster_centers)
         
     def visual_plot_Kmeans(self):
-        #Init the plot with the specified dimensions
-        fig = plt.figure(figsize=(6, 4))
+        try:
+            #Init the plot with the specified dimensions
+            fig = plt.figure(figsize=(6, 4))
 
-        #Colors uses a color map, which will produce an array of colors based on the number of labels there are
-        #We use set(k_means_labels) to get the unique labels
-        colors = plt.cm.tab10(np.linspace(0, 1, len(set(self.k_means_labels))))
+            #Colors uses a color map, which will produce an array of colors based on the number of labels there are
+            #We use set(k_means_labels) to get the unique labels
+            colors = plt.cm.tab10(np.linspace(0, 1, len(set(self.__k_means_labels))))
 
-        #Create a plot
-        ax = fig.add_subplot(1, 1, 1)
+            #Create a plot
+            ax = fig.add_subplot(1, 1, 1)
 
-        #plots the data points and centroids
-        #k will range from 0-3, which will match the possible clusters that each data point is in
-        for k, col in zip(range(len([[4, 4], [-2, -1], [2, -3], [1, 1]])), colors):
+            #plots the data points and centroids
+            #k will range from 0-3, which will match the possible clusters that each data point is in
+            for k, col in zip(range(len([[4, 4], [-2, -1], [2, -3], [1, 1]])), colors):
 
-            #Create a list of all data p., where the data p. that are  in the cluster (ex. cluster 0) are labeled as true, else - false.
-            my_members = (self.k_means_labels == k)
+                #Create a list of all data p., where the data p. that are  in the cluster (ex. cluster 0) are labeled as true, else - false.
+                my_members = (self.__k_means_labels == k)
 
-            #def the centroid or cluster center
-            cluster_center = self.k_means_cluster_centers[k]
+                #def the centroid or cluster center
+                cluster_center = self.__k_means_cluster_centers[k]
 
-            #plots the data p. with color col.
-            ax.plot(self.X[my_members, 0], self.X[my_members, 1], 'w', markerfacecolor=col, marker='.',ms=10)
+                #plots the data p. with color col.
+                ax.plot(self.__X[my_members, 0], self.__X[my_members, 1], 'w', markerfacecolor=col, marker='.',ms=10)
 
-            #Plots the centroids with specified color (with a darker outline)
-            ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,  markeredgecolor='k', markersize=6)
+                #Plots the centroids with specified color (with a darker outline)
+                ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,  markeredgecolor='k', markersize=6)
 
-        ax.set_title('KMeans')
+            ax.set_title('KMeans')
 
-        #Remove x-axis && y-axis ticks
-        ax.set_xticks(())
-        ax.set_yticks(())
-        plt.show()         
-        
+            #Remove x-axis && y-axis ticks
+            ax.set_xticks(())
+            ax.set_yticks(())
+            plt.show()         
+        except Exception:
+            print(f"Error in visualization plot Kmeans !!!")
+            
     def run(self):
         self.create_syntetic_data()
         self.visualizing_data()
